32caeaef68510ca056133b05224d47b829c8b176
kaushik.sst@gmail.com <kaushik.sst@gmail.com>
2025-03-10 12:20:01 +0000

    Added new feature for http connection

--- src/main/java/com/sai/jwttoken/Test.java
+++ src/main/java/com/sai/jwttoken/Test.java
@@ -1,15 +1,16 @@
 package com.sai.jwttoken;
 
+
 import java.net.CookieHandler;
 import java.net.CookieManager;
 import java.net.CookiePolicy;
 import java.net.URI;
-import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpClient.Redirect;
 import java.net.http.HttpRequest;
-import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
@@ -17,45 +18,34 @@ import java.security.cert.X509Certificate;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
-
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+public class Test {
+//    public static void main(String[] args) {
+//        try {
+//            HttpClient client = createHttpClientWithNoSSLValidation();
+//            Map<Object, Object> data = new HashMap<>();
+//            data.put("username", "username");
+//            data.put("password", "password");
+//
+//            HttpRequest request = HttpRequest.newBuilder()
+//                .uri(URI.create("https://stats-uat.servicenow.net/stats-api/api/authenticate"))
+//                .header("accept", "application/json, text/plain, */*")
+//                .header("content-type", "application/x-www-form-urlencoded")
+//                .POST(BodyPublishers.ofString(buildFormDataFromMap(data)))
+//                .build();
+//
+//            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+//            System.out.println("Response Code: " + response.statusCode());
+//            System.out.println("Response Body: " + response.body());
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 
-@RestController
-@RequestMapping("/jwt")
-public class Controller {
-
-    @GetMapping("/get")
-    public String getJwtTokeb() throws Exception{
-    	try {
-            HttpClient client = createHttpClientWithNoSSLValidation();
-            Map<Object, Object> data = new HashMap<>();
-            data.put("username", "username");
-            data.put("password", "password");
-
-            HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://stats-uat.servicenow.net/stats-api/api/authenticate"))
-                .header("accept", "application/json, text/plain, */*")
-                .header("content-type", "application/x-www-form-urlencoded")
-                .POST(BodyPublishers.ofString(buildFormDataFromMap(data)))
-                .build();
-
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println("Response Code: " + response.statusCode());
-            System.out.println("Response Body: " + response.body());
-            return response.toString();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "Something wrong";
-        }
-    }
-    
     private static String buildFormDataFromMap(Map<Object, Object> data) {
         return data.entrySet()
             .stream()
@@ -90,3 +80,4 @@ public class Controller {
             .build();
     }
 }
+
